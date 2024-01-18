defb3860145725906a7178a0126780ad10eaa946
fabiano-manjaro <fabiano.analyst@gmail.com>
2024-01-18 16:41:56 -0300

    create microservices e-mail success

--- src/main/java/com/ms/email/consumers/EmailConsumers.java
+++ src/main/java/com/ms/email/consumers/EmailConsumers.java
@@ -1,15 +1,31 @@
 package com.ms.email.consumers;
 
 import com.ms.email.dtos.EmailRecordDTO;
+import com.ms.email.model.EmailModel;
+import com.ms.email.repository.EmailRepository;
+import com.ms.email.service.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumers {
 
+    final EmailRepository emailRepository;
+    final EmailService emailService;
+
+    public EmailConsumers(EmailRepository emailRepository, EmailService emailService) {
+        this.emailRepository = emailRepository;
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO){
-        System.out.println(emailRecordDTO.emailTo());
+        //System.out.println(emailRecordDTO.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDTO, emailModel);
+        emailService.sendEmail(emailModel);
+
     }
 }
